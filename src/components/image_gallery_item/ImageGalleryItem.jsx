dfa0713cb47ea0f595b4c81577d63984314196de
krystianklimczak <krystianklimczak1996r.com>
2023-11-04 20:46:49 +0100

    change ImageGalleryItem class component to function component & format imports

--- src/components/image_gallery_item/ImageGalleryItem.jsx
+++ src/components/image_gallery_item/ImageGalleryItem.jsx
@@ -1,30 +1,15 @@
-// 'React.Component'
-import React, { Component } from 'react';
-
-// css modules
 import css from './ImageGalleryItem.module.css';
 
-// proptypes
-import PropTypes from 'prop-types';
-
-export default class ImageGalleryItem extends Component {
-  render() {
-    const { smallImage, alt, largeImage } = this.props;
-    return (
-      <li className={css.imageGalleryItem}>
-        <img
-          src={smallImage}
-          alt={alt}
-          data-source={largeImage}
-          className={css['imageGalleryItem-image']}
-        />
-      </li>
-    );
-  }
+export default function ImageGalleryItem(props) {
+  const { smallImage, largeImage, alt } = props;
+  return (
+    <li className={css.imageGalleryItem}>
+      <img
+        src={smallImage}
+        alt={alt}
+        data-source={largeImage}
+        className={css['imageGalleryItem-image']}
+      />
+    </li>
+  );
 }
-
-ImageGalleryItem.propTypes = {
-  smallImage: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
-  largeImage: PropTypes.string.isRequired,
-};
