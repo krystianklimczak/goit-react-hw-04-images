beb77e86b2868d4bcee2ba05b9cfd3d2be2705c1
krystianklimczak <krystianklimczak1996r.com>
2023-11-04 20:48:15 +0100

    add custom hook for images context with logic and proptypes

---
+++ src/hooks/ImagesContext.js
@@ -0,0 +1,193 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import PropTypes from 'prop-types';
+
+const { createContext, useContext, useState, useEffect } = require('react');
+
+const ImagesContext = createContext();
+
+export const useImages = () => useContext(ImagesContext);
+
+export const ImagesProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [onLastPage, setOnLastPage] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState('');
+  const [prevQuery, setPrevQuery] = useState('');
+  const [query, setQuery] = useState('');
+  const [modalImageURL, setModalImageURL] = useState('');
+
+  useEffect(() => {
+    if (query !== '') {
+      getInitialData();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  useEffect(() => {
+    if (currentPage !== 1) {
+      getInitialData();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage]);
+
+  useEffect(() => {
+    if (currentPage !== 1) {
+      window.scrollTo(0, document.body.scrollHeight);
+    }
+  }, [isLoading, currentPage]);
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const query = form.elements.query.value;
+
+    switch (query) {
+      case '':
+        Notiflix.Notify.warning(`Please fill out this field`);
+        break;
+      case prevQuery:
+        Notiflix.Notify.info(`You already search for the ${prevQuery}`);
+        break;
+      default:
+        // this.setState(
+        //   {
+        //     prevQuery: query,
+        //     currentPage: 1,
+        //     images: [],
+        //     query: query,
+        //   },
+        //   () => this.getInitialData()
+        // );
+        setPrevQuery(query);
+        setCurrentPage(1);
+        setImages([]);
+        setQuery(query);
+      // ????
+      // () => {
+      //   const [query, setQuery] = useState(query);
+      //   getInitialData();
+      // };
+    }
+  };
+
+  const handleClick = () => {
+    // this.setState(prevState => ({
+    //   currentPage: prevState.currentPage + 1,
+    // }));
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handleShowModal = event => {
+    if (event.target.nodeName !== 'IMG') {
+      return;
+    }
+
+    // this.setState({
+    //   isModalVisible: true,
+    //   modalImageURL: event.target.dataset.source,
+    // });
+
+    // ?????
+    setIsModalVisible(true);
+    setModalImageURL(event.target.dataset.source);
+  };
+
+  const handleEscapeKey = event => {
+    if (event.key === 'Escape') {
+      // this.setState({
+      //   isModalVisible: false,
+      // });
+      setIsModalVisible(false);
+    }
+  };
+
+  async function getInitialData() {
+    // const { query, currentPage } = this.state;
+    const searchParams = new URLSearchParams({
+      q: query,
+      page: currentPage,
+      key: '40298535-c3e5c72155b16daae721a7471',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      per_page: 12,
+    });
+
+    try {
+      // this.setState({ isLoading: true });
+      setIsLoading(true);
+      const response = await axios.get(
+        `https://pixabay.com/api/?${searchParams}`
+      );
+      const responseImages = await response.data.hits;
+      if (responseImages.length === 0) {
+        Notiflix.Notify.failure(
+          `There isn't any images with that query: "${query}"`
+        );
+      }
+      // this.setState(state => ({
+      //   images: [...state.images, ...responseImages],
+      //   onLastPage: responseImages.length < 12 ? true : false,
+      // }));
+      setImages([...images, ...responseImages]);
+      const isOnLastPage = responseImages.length < 12 ? true : false;
+      setOnLastPage(isOnLastPage);
+    } catch (error) {
+      // this.setState({ error: error.toString() });
+      setError(error.toString());
+    } finally {
+      // this.setState({ isLoading: false });
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <ImagesContext.Provider
+      value={{
+        isLoading,
+        isModalVisible,
+        onLastPage,
+        currentPage,
+        images,
+        error,
+        prevQuery,
+        query,
+        modalImageURL,
+        handleSubmit,
+        handleClick,
+        handleShowModal,
+        handleEscapeKey,
+      }}
+    >
+      {children}
+    </ImagesContext.Provider>
+  );
+};
+
+ImagesContext.Provider.propTypes = {
+  value: PropTypes.shape({
+    isLoading: PropTypes.bool.isRequired,
+    isModalVisible: PropTypes.bool.isRequired,
+    onLastPage: PropTypes.bool.isRequired,
+    currentPage: PropTypes.number.isRequired,
+    images: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        webformatURL: PropTypes.string.isRequired,
+        largeImageURL: PropTypes.string.isRequired,
+        tags: PropTypes.string.isRequired,
+      })
+    ),
+    error: PropTypes.string.isRequired,
+    prevQuery: PropTypes.string.isRequired,
+    query: PropTypes.string.isRequired,
+    modalImageURL: PropTypes.string.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+    handleClick: PropTypes.func.isRequired,
+    handleShowModal: PropTypes.func.isRequired,
+    handleEscapeKey: PropTypes.func.isRequired,
+  }),
+};
