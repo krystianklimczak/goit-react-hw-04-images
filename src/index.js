f47d3ad7651b00e4915a15a9b06c2eca49753025
krystianklimczak <krystianklimczak1996r.com>
2023-11-04 20:29:28 +0100

    add context provider for app & format imports

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-// 'React'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// components
 import App from 'components/App';
+import { ImagesProvider } from 'hooks/ImagesContext';
 
-// css
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ImagesProvider>
+      <App />
+    </ImagesProvider>
   </React.StrictMode>
 );
