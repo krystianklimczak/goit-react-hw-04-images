1729f5794695f480ec63cda8513e8c5020b3bb2b
krystianklimczak <krystianklimczak1996r.com>
2023-11-04 20:46:36 +0100

    change ImageGallery class component to function component & format imports

--- src/components/image_gallery/ImageGallery.jsx
+++ src/components/image_gallery/ImageGallery.jsx
@@ -1,39 +1,21 @@
-// 'React.Component'
-import React, { Component } from 'react';
-
-// components
+import { useImages } from 'hooks/ImagesContext';
 import ImageGalleryItem from 'components/image_gallery_item/ImageGalleryItem';
 
-// css modules
 import css from './ImageGallery.module.css';
 
-// proptypes
-import PropTypes from 'prop-types';
+export default function ImageGallery() {
+  const { images, handleShowModal } = useImages();
 
-export default class ImageGallery extends Component {
-  render() {
-    const { images, handleShowModal } = this.props;
-    return (
-      <ul className={css.imageGallery} onClick={handleShowModal}>
-        {images.map(image => (
-          <ImageGalleryItem
-            key={image.id}
-            smallImage={image.webformatURL}
-            largeImage={image.largeImageURL}
-            alt={image.tags}
-          />
-        ))}
-      </ul>
-    );
-  }
+  return (
+    <ul className={css.imageGallery} onClick={handleShowModal}>
+      {images.map(image => (
+        <ImageGalleryItem
+          key={image.id}
+          smallImage={image.webformatURL}
+          largeImage={image.largeImageURL}
+          alt={image.tags}
+        />
+      ))}
+    </ul>
+  );
 }
-
-ImageGallery.propTypes = {
-  images: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      webformatURL: PropTypes.string.isRequired,
-      largeImageURL: PropTypes.string.isRequired,
-    })
-  ),
-};
